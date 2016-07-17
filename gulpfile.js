7cfb94b47e70101ce2c6d3523ace635666900f62
Andrey Bakhvalov <bakhvalov.andrey@gmail.com>
2016-07-17 17:07:51 +0300

    optimize

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var gulp = require('gulp'),
 	pngquant = require('imagemin-pngquant'),
 	rimraf = require('rimraf'),
 	browserSync = require('browser-sync'),
+	htmlmin = require('gulp-htmlmin'),
 	reload = browserSync.reload;
 
 var path = {
@@ -57,6 +58,12 @@ var config = {
 gulp.task('html:build', function () {
 	gulp.src(path.src.html) //Выберем файлы по нужному пути
 		.pipe(rigger()) //Прогоним через rigger
+		.pipe(htmlmin({
+			collapseWhitespace: true,
+			conservativeCollapse: true,
+			minifyJS: true,
+			removeEmptyAttributes: true
+		}))
 		.pipe(gulp.dest(path.dest.html)) //Выплюнем их в папку build
 		.pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
 });
